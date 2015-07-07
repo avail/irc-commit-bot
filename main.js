76b4fabae84e82c2296bb5438a141196f7bbeb55
avail <avail@pomf.se>
2015-07-07 10:26:12 +0200

    github: implement pull request hook

--- main.js
+++ main.js
@@ -208,33 +208,65 @@ app.post("/git.json", jp, function (req, res) {
 
         } else if (req.headers["x-gitlab-event"] == "Merge Request Hook" || req.headers["x-github-event"] == "pull_request") {
 
-            var type = "";
-            switch(req.body["object_attributes"]["state"].toLowerCase()) {
-                case "opened":
-                type = "Opened";
-                break;
+            if (req.headers["x-gitlab-event"] != null) {
+                switch(req.body["object_attributes"]["state"].toLowerCase()) {
+                    case "opened":
+                    var type = "Opened";
+                    break;
 
-                case "merged":
-                type = "Merged";
-                break;
+                    case "merged":
+                    var type = "Merged";
+                    break;
 
-                case "closed":
-                type = "Closed";
-                break;
+                    case "closed":
+                    var type = "Closed";
+                    break;
 
-                case "reopened":
-                type = "Reopened";
-                break;
+                    case "reopened":
+                    var type = "Reopened";
+                    break;
+                }
+
+                var action = req.body["object_attributes"]["action"];
+                var merge_url = req.body["object_attributes"]["url"];
+                var merge_id = req.body["object_attributes"]["iid"];
+                var merge_title = req.body["object_attributes"]["title"];
+                var merge_user = req.body["user"]["name"];
+
+            } else if (req.headers["x-github-event"]) {
+
+                switch(req.body["action"].toLowerCase()) {
+                    case "opened":
+                    var type = "Opened";
+                    break;
+
+                    case "closed":
+                    var type = "Closed";
+                    break;
+
+                    case "reopened":
+                    var type = "Reopened";
+                    break;
+                }
+
+                if (req.body["pull_request"]["merged"] == true)
+                    type = "Merged";
+
+                var action = req.body["action"];
+                var merge_url = req.body["pull_request"]["html_url"];
+                var merge_id = req.body["pull_request"]["number"];
+                var merge_title = req.body["pull_request"]["title"];
+                var merge_user = req.body["pull_request"]["user"]["login"];
             }
 
-            if (req.body["object_attributes"]["action"] == "open" || req.body["object_attributes"]["action"] == "close" || req.body["object_attributes"]["action"] == "reopen") {
-                isgd.shorten(req.body["object_attributes"]["url"], function(resp) {
+            if (action == "open" || action == "close" || action == "reopen" || action == "opened" || action == "closed" || action == "reopened" || type == "Merged") {
+                isgd.shorten(merge_url, function(resp) {
                     for (var channel of config.channels) {
                         bot.say(channel, util.format("\x02\x0306Merge request\x03\x02: \x02#%d\x02 \x02\x0303%s\x03\x02 - %s by %s - %s",
-                            req.body["object_attributes"]["iid"],
-                            req.body["object_attributes"]["title"],
+                            merge_id,
+                            merge_title,
                             type,
-                            req.body["user"]["name"],
+                            merge_user,
                             resp));
                     }
                 });
