18e7ca9f0b8331f8f983dbad1d2419986ae0a69a
Asnivor <coding@asnitech.co.uk>
2019-02-17 15:09:04 +0000

    formatting changes

--- main.js
+++ main.js
@@ -509,7 +509,7 @@ function handleAPI(req, res) {
 // GitHub related hooks
 function handleGitHub(req, res) {
     for (var channel of channels) {
-        bot.say(channel, req.headers["x-github-event"] + " : " + req.body["action"]);
+        bot.say(channel, "DEBUG: " + req.headers["x-github-event"] + " : " + req.body["action"]);
     }
 
     // action
